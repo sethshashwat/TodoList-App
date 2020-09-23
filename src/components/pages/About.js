20415c125f156735079432d7104ad07a9907cb5c
Shashwat Seth <shashwat.seth.131999@gmail.com>
2020-09-23 20:51:51 +0530

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList app is created by Shashwat Seth using ReactJS. </p>
+        </React.Fragment>
+    )
+}
+
+
+export default About
